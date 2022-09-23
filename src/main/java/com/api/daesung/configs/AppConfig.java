8135788270b4f42326fe7faa595bb17a71bd8c9f
sorojava <soro.java@gmail.com>
2022-09-23 17:44:52 +0900

    spring security 기본 설정 중 해결

--- src/main/java/com/api/daesung/configs/AppConfig.java
+++ src/main/java/com/api/daesung/configs/AppConfig.java
@@ -3,6 +3,8 @@ package com.api.daesung.configs;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig{
@@ -12,9 +14,9 @@ public class AppConfig{
         return new ModelMapper();
     }
 
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 }
